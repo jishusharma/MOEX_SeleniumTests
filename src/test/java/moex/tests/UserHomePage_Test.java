fe4c1a176d8222b2cb4f44c91e71f95d6069bf20
sei <sei@e-ngineers.com>
2019-08-07 11:51:35 +0300

    fix warnings add file check and delete

--- src/test/java/moex/tests/UserHomePage_Test.java
+++ src/test/java/moex/tests/UserHomePage_Test.java
@@ -5,8 +5,9 @@ import moex.pages.LoginPage;
 import moex.pages.UserHomePage;
 import org.testng.Assert;
 
-import java.io.*;
-import java.text.ParseException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.time.LocalDateTime;
 
 public class UserHomePage_Test extends AutomationBaseClass {
@@ -25,32 +26,28 @@ public class UserHomePage_Test extends AutomationBaseClass {
         userHomePage = loginPage.loginWithCorrectCredentials(LoginPage.username, LoginPage.password);
         WaitForElementToAppear(UserHomePage.userImage);
         userHomePage.switchToWorkStation();
-        Assert.assertTrue(UserHomePage.traderCombobox.isDisplayed(),"Trader not displayed!");
+        Assert.assertTrue(UserHomePage.traderCombobox.isDisplayed(), "Trader not displayed!");
 
     }
 
     //This test will fail if no records are found for the current date
     @org.testng.annotations.Test(priority = 4)
-    public void test_getDealsForToday() throws ParseException, InterruptedException {
+    public void test_getDealsForToday() throws InterruptedException {
         LocalDateTime date = LocalDateTime.now();
         LocalDateTime startDate = LocalDateTime.of(date.getYear(), date.getMonth(), date.getDayOfMonth(), 0, 0, 0);
         LocalDateTime endDate = LocalDateTime.of(date.getYear(), date.getMonth(), date.getDayOfMonth(), 23, 59, 59);
         getDeals(startDate, endDate);
-        Assert.assertTrue(userHomePage.doesTableHasRows());
-        userHomePage.saveFile();
-        checkListDealsFile();
+        checkDeals();
     }
 
     //This test has been additionally written as the current date test was not fetching any results to assert for
     @org.testng.annotations.Test(priority = 5)
-    public void test_getDealsLastMonth() throws ParseException, InterruptedException {
+    public void test_getDealsLastMonth() throws InterruptedException {
         LocalDateTime date = LocalDateTime.now();
         LocalDateTime startDate = LocalDateTime.of(date.getYear(), date.getMonth().minus(1), date.getDayOfMonth(), 0, 0, 0);
         LocalDateTime endDate = LocalDateTime.of(date.getYear(), date.getMonth(), date.getDayOfMonth(), 23, 59, 59);
         getDeals(startDate, endDate);
-        Assert.assertTrue(userHomePage.doesTableHasRows());
-        userHomePage.saveFile();
-        checkListDealsFile();
+        checkDeals();
     }
 
     private void getDeals(LocalDateTime startDate, LocalDateTime endDate) {
@@ -59,6 +56,12 @@ public class UserHomePage_Test extends AutomationBaseClass {
         WaitForElementToAppear(UserHomePage.userImage);
         userHomePage.switchToWorkStation();
         userHomePage.getDealsForDateRange(startDate, endDate);
+    }
+
+    private void checkDeals() throws InterruptedException {
+        Assert.assertTrue(userHomePage.doesTableHasRows());
+        userHomePage.saveFile();
+        checkListDealsFile();
     }
 
     private void checkListDealsFile() {
